5e1630459b46503f5f95408be9f27598c45030b2
luigilalo <luigilaloe1@gmail.com>
2023-11-14 17:39:24 +0100

    Layout Improvements 2

--- js/main.js
+++ js/main.js
@@ -16,16 +16,14 @@ btnGenerate.addEventListener("click",
         console.log("Km:", km);
 
         let ticketPrice = km * 0.21;
-        console.log("Il prezzo del biglietto sarà di", ticketPrice)
 
         if (age == "Minorenne") {
-            ticketPrice = ticketPrice - (ticketPrice * 0.2);
-            console.log("Il prezzo per un minorenne sarà di", ticketPrice);
+          ticketPrice = ticketPrice - (ticketPrice * 0.2);
+            console.log("Il prezzo per un minorenne sarà di" + " " + ticketMinor);
         }
         else if (age == "Over65") {
-            ticketPrice = ticketPrice - (ticketPrice * 0.4);
-            console.log("Il prezzo per un Over65 sarà di", ticketPrice);
+            ticketPrice = ticketPrice - (ticketPrice * 0.2);
+            console.log("Il prezzo per un Over65 sarà di" + " " + ticketOver65);
         }
-
     }
 )
